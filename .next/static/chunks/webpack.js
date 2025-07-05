56292ff3e9741c98a8cf37a24890475e1691b884
Amer Al.Adas <amer.adas@icloud.com>
2025-07-05 15:41:56 +0300

    Fix SEO function call and client component errors

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("9e69e66ae5b17713")
+/******/ 		__webpack_require__.h = () => ("cc978654ae64efbc")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
